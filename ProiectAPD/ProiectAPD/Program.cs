5a382470e17d58e4e1cee19ea862354945cbbaf9
Rares <raressclipceadaniel@yahoo.com>
2024-05-21 19:21:59 +0300

    Added LINQ Parallelization

--- ProiectAPD/ProiectAPD/Program.cs
+++ ProiectAPD/ProiectAPD/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Diagnostics;
 using System.Drawing;
+using System.Linq;
 using System.Threading.Tasks;
 
 class Program
@@ -14,7 +15,8 @@ class Program
         Stopwatch sequentialStopwatch = Stopwatch.StartNew();
         string outputFilePath = "D:\\Facultate\\APD\\Proiect Curs\\ProiectAPD\\ProiectAPD\\output.jpg";
         //ProcessImageSequentially(originalImage, outputFilePath);
-        ProcessImageInParallelAsync(originalImage, outputFilePath);
+        //ProcessImageInParallelAsync(originalImage, outputFilePath).Wait();
+        ProcessImageWithPLINQ(originalImage, outputFilePath);
         sequentialStopwatch.Stop();
         Console.WriteLine("Processing time: " + sequentialStopwatch.ElapsedMilliseconds + " ms");
     }
@@ -160,4 +162,153 @@ class Program
         });
     }
 
+    static void ProcessImageWithPLINQ(Bitmap original, string outputFilePath)
+    {
+        int width = 200;
+        int height = 200;
+
+        Bitmap result = new Bitmap(width, height);
+
+        // Convert to black and white using PLINQ
+        var bwImage = ConvertToBlackAndWhitePLINQ(original);
+
+        // Resize using PLINQ
+        var resizedImage = ResizePLINQ(bwImage, width, height);
+
+        // Convert blue to red using PLINQ
+        ConvertBlueToRedPLINQ(resizedImage);
+
+        // Save the processed image
+        resizedImage.Save(outputFilePath);
+        Console.WriteLine("Processed image saved to: " + outputFilePath);
+    }
+
+    static Bitmap ConvertToBlackAndWhitePLINQ(Bitmap original)
+    {
+        int width = original.Width;
+        int height = original.Height;
+
+        // Extract pixel data into a buffer
+        Color[,] pixels = new Color[width, height];
+        for (int x = 0; x < width; x++)
+        {
+            for (int y = 0; y < height; y++)
+            {
+                pixels[x, y] = original.GetPixel(x, y);
+            }
+        }
+
+        Color[,] bwPixels = new Color[width, height];
+
+        // Process the pixels using PLINQ
+        var bwPixelData = Enumerable.Range(0, width)
+            .SelectMany(x => Enumerable.Range(0, height), (x, y) => new { x, y })
+            .AsParallel()
+            .Select(p =>
+            {
+                Color pixelColor = pixels[p.x, p.y];
+                int avgColor = (pixelColor.R + pixelColor.G + pixelColor.B) / 3;
+                return new { p.x, p.y, bwColor = Color.FromArgb(avgColor, avgColor, avgColor) };
+            })
+            .ToList();
+
+        foreach (var p in bwPixelData)
+        {
+            bwPixels[p.x, p.y] = p.bwColor;
+        }
+
+        Bitmap bwImage = new Bitmap(width, height);
+        for (int x = 0; x < width; x++)
+        {
+            for (int y = 0; y < height; y++)
+            {
+                bwImage.SetPixel(x, y, bwPixels[x, y]);
+            }
+        }
+
+        return bwImage;
+    }
+
+    static Bitmap ResizePLINQ(Bitmap original, int newWidth, int newHeight)
+    {
+        int width = original.Width;
+        int height = original.Height;
+
+        // Extract pixel data into a buffer
+        Color[,] pixels = new Color[width, height];
+        for (int x = 0; x < width; x++)
+        {
+            for (int y = 0; y < height; y++)
+            {
+                pixels[x, y] = original.GetPixel(x, y);
+            }
+        }
+
+        Color[,] resizedPixels = new Color[newWidth, newHeight];
+
+        // Process the pixels using PLINQ
+        var resizedPixelData = Enumerable.Range(0, newWidth)
+            .SelectMany(x => Enumerable.Range(0, newHeight), (x, y) => new { x, y })
+            .AsParallel()
+            .Select(p =>
+            {
+                int origX = (int)((double)p.x / newWidth * width);
+                int origY = (int)((double)p.y / newHeight * height);
+                Color pixelColor = pixels[origX, origY];
+                return new { p.x, p.y, pixelColor };
+            })
+            .ToList();
+
+        foreach (var p in resizedPixelData)
+        {
+            resizedPixels[p.x, p.y] = p.pixelColor;
+        }
+
+        Bitmap resizedImage = new Bitmap(newWidth, newHeight);
+        for (int x = 0; x < newWidth; x++)
+        {
+            for (int y = 0; y < newHeight; y++)
+            {
+                resizedImage.SetPixel(x, y, resizedPixels[x, y]);
+            }
+        }
+
+        return resizedImage;
+    }
+
+    static void ConvertBlueToRedPLINQ(Bitmap image)
+    {
+        int width = image.Width;
+        int height = image.Height;
+
+        // Extract pixel data into a buffer
+        Color[,] pixels = new Color[width, height];
+        for (int x = 0; x < width; x++)
+        {
+            for (int y = 0; y < height; y++)
+            {
+                pixels[x, y] = image.GetPixel(x, y);
+            }
+        }
+
+        // Process the pixels using PLINQ
+        var pixelData = Enumerable.Range(0, width)
+            .SelectMany(x => Enumerable.Range(0, height), (x, y) => new { x, y })
+            .AsParallel()
+            .Select(p =>
+            {
+                Color pixelColor = pixels[p.x, p.y];
+                if (pixelColor.B > 0)
+                {
+                    pixelColor = Color.FromArgb(pixelColor.R, pixelColor.G, 255);
+                }
+                return new { p.x, p.y, pixelColor };
+            })
+            .ToList();
+
+        foreach (var p in pixelData)
+        {
+            image.SetPixel(p.x, p.y, p.pixelColor);
+        }
+    }
 }
